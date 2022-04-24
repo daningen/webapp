a5c72b3bbf4099669529d273feef444ba2ed81b8
daningen <daningpublic@gmail.com>
2022-04-24 17:08:38 +0200

    Explain what changes I did

--- kmom10/proj/www/js/views/delayDetails.js
+++ kmom10/proj/www/js/views/delayDetails.js
@@ -330,6 +330,7 @@ let delayDetails = {
                     //     href: `#!/heatView`
                     // }, " heat map som visar alla förseningar"),
                     // console.log("fck"),
+                    //testar kommenterar
                     m("h1","Detaljer om försening"),
                     m("p", getDetailsX(vnode.attrs.id.trim())),
                 ),
